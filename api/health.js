659f0c646eacd99dab7da9bf33ea24bfaefb5613
jimhpga <jimh@pga.com>
2025-08-29 15:00:10 -0700

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,8 +1,1 @@
-export default function handler(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).send(JSON.stringify({
-    ok: true,
-    service: 'vcai',
-    t: Date.now()
-  }, null, 2));
-}
+
