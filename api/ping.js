1e6f7adbc90b015e57a90c9842cefd684c754458
jimhpga <jimh@pga.com>
2025-09-06 15:31:27 -0700

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,5 +1,5 @@
 // api/ping.js
-export const config = { runtime: 'nodejs20.x', maxDuration: 10, regions: ['pdx1','sfo1'] };
+export const config = { runtime: 'nodejs', maxDuration: 10, regions: ['pdx1','sfo1'] };
 export default async function handler(req, res) {
   res.status(200).json({ ok:true, method:req.method, t:Date.now() });
 }
