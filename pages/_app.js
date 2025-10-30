c7235025bed39a180e911e008cb673609e54b807
Your Name <you@example.com>
2025-10-30 16:05:15 -0700

    ui: add global dark styles + wire to _app

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="stylesheet" href="/styles/globals.css" />
+      <Component {...pageProps} />
+    </>
+  );
+}
