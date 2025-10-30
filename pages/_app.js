5cffa77542b81187db4e4c823e16b40512383a04
Your Name <you@example.com>
2025-10-30 16:44:14 -0700

    wire global styles via _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,5 @@
+﻿import "../styles/globals.css";
+
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="stylesheet" href="/styles/globals.css" />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
