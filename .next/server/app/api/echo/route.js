477bf1a40cda19cda5585e61fa3baabab9471559
Jim Hartnett <jimhartnettgolf@gmail.com>
2025-12-20 16:04:25 -0800

    Stop tracking .next build output

--- .next/server/app/api/echo/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/echo/route.js")
-R.c("server/chunks/[root-of-the-server]__cc24fc84._.js")
-R.c("server/chunks/[root-of-the-server]__8f5ebbc3._.js")
-R.c("server/chunks/_next-internal_server_app_api_echo_route_actions_89a204e2.js")
-R.m(74088)
-module.exports=R.m(74088).exports
--- .next/server/app/api/ping/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/ping/route.js")
-R.c("server/chunks/[root-of-the-server]__6e707ba6._.js")
-R.c("server/chunks/[root-of-the-server]__8f5ebbc3._.js")
-R.c("server/chunks/_next-internal_server_app_api_ping_route_actions_8c5eb44e.js")
-R.m(88551)
-module.exports=R.m(88551).exports
