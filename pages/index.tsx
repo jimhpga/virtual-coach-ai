f167b6545b00579a05cb9b207a95019742cb5ffc
Jim Hartnett <jimhartnettgolf@gmail.com>
2025-12-20 18:36:01 -0800

    Make homepage render upload page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,1 @@
-﻿import { useEffect } from "react";
-import { useRouter } from "next/router";
-
-export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace("/upload");
-  }, [router]);
-
-  return null;
-}
+﻿export { default } from "./upload";
