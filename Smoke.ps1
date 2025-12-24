79bc24a67c040561d88cc8e8d747f56b968fbc23
Jim Hartnett <jimhartnettgolf@gmail.com>
2025-12-24 07:30:21 -0800

    Add quick smoke test script

---
+++ Smoke.ps1
@@ -0,0 +1,5 @@
+﻿$base="http://localhost:3000"
+"/","/upload","/report" | %{
+  try { $r=Invoke-WebRequest -UseBasicParsing ($base+$_) -TimeoutSec 5; "{0,-10} {1}" -f $_,$r.StatusCode }
+  catch { "{0,-10} FAIL {1}" -f $_,$_.Exception.Message }
+}
