01353289815653ea4886bce2e7aea521576d28b8
Jim Hartnett <jimhartnettgolf@gmail.com>
2026-02-11 08:05:23 -0800

    Dev: allow localhost/127.0.0.1 origins for Next dev

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
-﻿/** TEMP: unblock build on Vercel/CI by skipping ESLint + TS typecheck */
+/** TEMP: unblock build on Vercel/CI by skipping ESLint + TS typecheck */
 module.exports = {
+  allowedDevOrigins: ["http://127.0.0.1:3000","http://localhost:3000","http://192.168.1.145:3000"],
+
   
   webpack: (config, { dev }) => {
     if (dev) config.cache = false;
     return config;
-  },allowedDevOrigins: ['http://127.0.0.1:3000','http://localhost:3000','http://192.168.1.145:3000'],
+  },
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 };
